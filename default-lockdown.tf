428b408ba8a417ab9c66ed1503e96d43162aad0a
David Thornton <dthornton@carrotinsights.com>
2019-04-03 14:22:31 -0400

    added def sg for def vpc.

--- default-lockdown.tf
+++ default-lockdown.tf
@@ -18,6 +18,24 @@ resource "aws_default_vpc" "default" {
   }
 }
 
+resources "aws_default_security_group" "default_sg" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output default_vpc_id {
  value = "aws_default_vpc.default.id"
 }
